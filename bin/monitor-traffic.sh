a99325d17aba3cca586b3b9c0661de6dc18f012d
c-braz <53799728+c-braz@users.noreply.github.com>
2022-11-23 13:28:41 -0500

    Update monitor-traffic.sh

--- bin/monitor-traffic.sh
+++ bin/monitor-traffic.sh
@@ -1,1 +1,1 @@
-tcpdump -i eth0 -s 0 -nvvl | egrep --color=always '\[R.\]|$' | GREP_COLOR="1;36" egrep --color=always '([0-9]{1,3}[\.]){3}[0-9]{1,3}|$'
+sudo -E xfce4-terminal --geometry=100x5-0-0 -x /bin/zsh -c "tcpdump -i eth0 -s 0 -nvvl | egrep --color=always '\[R.\]|$' | GREP_COLOR=\"1;36\" egrep --color=always '([0-9]{1,3}[\.]){3}[0-9]{1,3}|$'; /bin/zsh" &
