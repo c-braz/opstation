a545845cb2c7d22cc3fe3f5eedcfeba90556d15d
c-braz <53799728+c-braz@users.noreply.github.com>
2022-11-23 15:09:06 -0500

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
 # User input to build directory structure
 read -p "OP_Name: " opname
 echo "Building $opname directories"
-
 mkdir $HOME/$opname
 mkdir $HOME/$opname/logs
 mkdir $HOME/$opname/tools
@@ -9,10 +9,12 @@ mkdir $HOME/$opname/tools
 cp -R bin/ ~/$opname/.
 cp etc/terminalrc $HOME/.config/xfce4/terminal/.
 
+echo "export opname=$opname" >> $HOME/.zshrc
 echo "export PATH=$PATH:$HOME/$opname/bin" >> $HOME/.zshrc
 
 echo "Following directories were created..."
 find $HOME/$opname
 
+cd $HOME/$opname
 $HOME/$opname/bin/monitor-traffic.sh
 xfce4-terminal --geometry=80x40-0+35 -x /bin/zsh -c "vim $HOME/$opname/logs/notes.txt ; /bin/zsh" &
