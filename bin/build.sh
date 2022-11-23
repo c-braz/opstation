66b7b1471db4b67ef53ae8c59a9a3d0e350965f7
c-braz <53799728+c-braz@users.noreply.github.com>
2022-11-23 11:57:38 -0500

    Create build.sh

---
+++ bin/build.sh
@@ -0,0 +1,15 @@
+# User input to build directory structure
+read -p "OP_Name: " opname
+echo "Building $opname directories"
+
+mkdir ~/$opname
+mkdir ~/$opname/logs
+mkdir ~/$opname/tools
+
+cp -R bin/ ~/$opname/.
+cp etc/ terminalrc ~/.config/xfce4/terminal/.
+
+echo "Following directories were created..."
+find ~/$opname
+
+xfce4-terminal -H -e ./bin/monitor-traffic.sh
